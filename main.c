7f98f69d90bdc94c2cccfa9e201913567a195eab
Thomas GERMAIN <thgermai@e2r5p22.42.fr>
2019-11-22 14:11:32 +0100

    push

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thomasgermain <thomasgermain@student.42    +#+  +:+       +#+        */
+/*   By: thgermai <thgermai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 09:46:03 by thgermai          #+#    #+#             */
-/*   Updated: 2019/11/21 22:00:05 by thomasgerma      ###   ########.fr       */
+/*   Updated: 2019/11/22 13:52:28 by thgermai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,27 @@
 
 int main(int ac, char **av)
 {
-	int fd1;
+	int fd;
 	int ret;
 	int line;
 	char *buff;
 
 	(void)ac;
 	line = 0;
-	fd1 = open(av[1], O_RDWR);
-	while ((ret = get_next_line(fd1, &buff)) > 0)
+	fd = open(av[1], O_RDWR);
+	while ((ret = get_next_line(fd, &buff)) > 0)
 	{
 		printf("[Return: %d] Line #%d: %s\n", ret, ++line, buff);
 		free(buff);
 	}
 	printf("[Return: %d] Line #%d: %s\n", ret, ++line, buff);
 	if (ret == -1)
-		printf("-----------\nError\n");
+		printf("-----------\nError\n\n");
 	else if (ret == 0)
-		printf("-----------\nEnd of file\n");
+		printf("-----------\nEnd of file\n\n");
 	if (buff)
 		free(buff);
+	close(fd);
+	system("leaks a.out");
 	return (0);
 }
